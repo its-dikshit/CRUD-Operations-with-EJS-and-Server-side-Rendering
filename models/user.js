f23a7d26c6eb755216a8daa11875d37fa8170083
Vishnu Dixit <116336112+its-dikshit@users.noreply.github.com>
2024-09-15 11:39:19 +0530

    Add files via upload
    This project demonstrates how to implement full CRUD (Create, Read, Update, Delete) operations using EJS (Embedded JavaScript) for server-side rendering in a Node.js/Express.js application. The application includes a dynamic user interface that displays data from a database, allowing users to create, edit, and delete entries.

---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose =require("mongoose");
+mongoose.connect("mongodb://localhost/testapp1"); 
+
+const userSchema= mongoose.Schema({
+    image: String,
+    email:String,
+    name: String
+})
+module.exports= mongoose.model('user',userSchema)
